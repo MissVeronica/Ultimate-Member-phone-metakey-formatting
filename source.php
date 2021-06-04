8fb8416c3b2817b883769f4a05213391fe5eb195
MissVeronica <veronica@tranza.se>
2021-06-04 13:45:48 +0200

    Update source.php

--- source.php
+++ source.php
@@ -1,8 +1,7 @@
 <?php
 
-// Version 1.0  
-// Date June 03 2021
-// https://github.com/MissVeronica/Ultimate-Member-phone-metakey-formatting
+// Version 1.0.1  
+// Date June 04 2021
 
 add_filter( 'um_view_field_value_text', 'my_custom_view_field_value', 10, 2);
 
@@ -43,17 +42,20 @@ function format_phone_string( $raw_number ) {
     unset( $arr_number[0] );                                // remove the dummy value so now the array keys start at 1    
     $num_number = count( $arr_number );                     // get the number of numbers in the number    
     $phone_number = '';
-    for ( $x = $num_number; $x >= 0; $x-- ) {               // loop through each number backward starting at the end
-        if ( $x === $num_number - 4 ) {                     // before the fourth to last number
-            $phone_number = "-" . $phone_number;
+    
+    if( $num_number > 0 ) {
+        for ( $x = $num_number; $x >= 0; $x-- ) {           // loop through each number backward starting at the end
+            if ( $x === $num_number - 4 ) {                 // before the fourth to last number
+                $phone_number = "-" . $phone_number;
+            }
+            else if ( $x === $num_number - 7 && $num_number > 7 ) { // before the seventh to last number and only if the number is more than 7 digits
+                $phone_number = ") " . $phone_number;
+            }
+            else if ( $x === $num_number - 10 ) {            // before the tenth to last number
+                $phone_number = "(" . $phone_number;
+            }        
+            $phone_number = $arr_number[$x] . $phone_number; // concatenate each number (possibly with modifications) back on
         }
-        else if ( $x === $num_number - 7 && $num_number > 7 ) { // before the seventh to last number and only if the number is more than 7 digits
-            $phone_number = ") " . $phone_number;
-        }
-        else if ( $x === $num_number - 10 ) {               // before the tenth to last number
-            $phone_number = "(" . $phone_number;
-        }        
-        $phone_number = $arr_number[$x] . $phone_number;    // concatenate each number (possibly with modifications) back on
     }
     return array( $country_code, $phone_number, $extension );
 }
@@ -71,7 +73,7 @@ function um_custom_validate_mobile_number( $key, $array, $args ) {
 
     //  A regex for a 7 or 10 digit number, with extensions allowed, delimiters are spaces, dashes, or periods
     //  https://stackoverflow.com/questions/123559/how-to-validate-phone-numbers-using-regex
-    
+
     if ( isset( $args[$key] )) {
         if( !preg_match( "/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/i", $args[$key] )) { 
             UM()->form()->add_error( $key, __( 'Please enter valid Mobile/Phone Number.', 'ultimate-member' ));
